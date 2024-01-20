05e79cb139ae9e5f036146c1e906cb13d82a81f4
ErickMeucci <144945118+ErickMeucci@users.noreply.github.com>
2024-01-20 02:04:51 -0300

    CRIANDO O BANCO DE DADOS
    Começo do banco de dados e criação da lista

--- App.py
+++ App.py
@@ -1,20 +1,72 @@
 from tkinter import *
 from tkinter import ttk
-import sqlite3 
+import sqlite3 as lite
 
 Janela = Tk()
-class funcao():
-    def conecta_bd(self):
-        self.conn = sqlite3.connect("gastos")
-        self.cursor = self.conn.cursor(); print("Conectando ao banco de dados")
-    def desconecta(self):
-        self.conn.close(); print("Desconecta")
-    def montatabela(self):
-        self.conecta_bd
-        ##Criar tabela
-        self.cursor.execute()
 
-class aplicacao():
+class Funcoes():
+    def limpatelaframe1(self):
+        self.psentrada.delete(0, END)
+        self.pientrada.delete(0, END)
+        self.pdentrada.delete(0, END)
+        self.pimentrada.delete(0, END)
+        self.plazer.delete(0, END)
+    def limpatelaframe2(self):
+        self.einvestimentos.delete(0, END)
+        self.vinvestimentos.delete(0, END)
+        self.dinvestimentos.delete(0, END)
+    def limpatelaframe3(self):
+        self.elazer.delete(0, END)
+        self.vlazer.delete(0, END)
+        self.dlazer.delete(0, END)
+    def limpatelaframe4(self):
+        self.edespesas.delete(0, END)
+        self.vdespesas.delete(0, END)
+        self.ddespesas.delete(0, END) 
+    def limpatelaframe5(self):
+        self.eimprovisos.delete(0, END)
+        self.vimprovisos.delete(0, END)
+        self.dimprovisos.delete(0, END) 
+    def conecta_bd(self):
+        self.conn = lite.connect("pagamentos.bd")
+        self.cursor = self.conn.cursor(); print('conecta banco de dados')
+    def desconecta_bd(self):
+        self.conn.close(); print('desconecta banco de dados')
+    def montatabela(self):
+        self.conecta_bd()
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS pagamentos (cod INTEGER PRIMARY KEY, descricao CHAR(40) NOT NULL, valor INTEGER(20) NOT NULL, data INTEGER(20) NOT NULL) """)
+        self.conn.commit();print("banco de dados criado")
+        self.desconecta_bd()
+    def addinvestimentos(self):
+        self.investimentos = self.einvestimentos.get()
+        self.investimentos1 = self.vinvestimentos.get()
+        self.investimentos2 = self.dinvestimentos.get()
+        self.conecta_bd()
+        self.cursor.execute("""INSERT INTO pagamentos (descricao, valor, data) VALUES (?,?,?)""",(self.investimentos,self.investimentos1,self.investimentos2))
+        self.conn.commit()
+        self.desconecta_bd
+        self.selectlista()
+        self.limpatelaframe2()
+    def addlazer(self):
+        self.lazer1 = self.elazer.get()
+        self.lazer2 = self.vlazer.get()
+        self.lazer3 = self.dlazer.get()
+        self.conecta_bd()
+        self.cursor.execute("""INSERT INTO pagamentos (descricao, valor, data) VALUES (?,?,?)""",(self.lazer1,self.lazer2,self.lazer3))
+        self.conn.commit()
+        self.desconecta_bd
+        self.selectlista()
+        self.limpatelaframe3()
+    def selectlista(self):
+        self.listagasto.delete(*self.listagasto.get_children())
+        self.conecta_bd()
+        listas = self.cursor.execute("""SELECT cod, descricao, valor, data FROM pagamentos ORDER BY data ASC;""")
+        for i in listas:
+            self.listagasto.insert("",END, values=i)
+        self.desconecta_bd()
+
+
+class aplicacao(Funcoes):
     def __init__(self):
         self.Janela = Janela
         self.tela()
@@ -22,6 +74,8 @@ class aplicacao():
         self.botoes()
         self.textosentradas()
         self.lista()
+        self.montatabela()
+        self.selectlista()
         Janela.mainloop()  
     def tela(self):
         self.Janela.title("Controle Finanças")
@@ -49,27 +103,31 @@ class aplicacao():
         #botao para adcionar as porcentagens
         self.bt_f1 = Button(self.frame_1, text="Adicionar",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'))
         self.bt_f1.place(relx=0.89, rely=0.05,relwidth=0.1,relheight=0.40)
-        self.bta_f1 = Button(self.frame_1, text="Limpar",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'))
+        self.bta_f1 = Button(self.frame_1, text="Limpar",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'),command=self.limpatelaframe1)
         self.bta_f1.place(relx=0.89, rely=0.55,relwidth=0.1,relheight=0.40)
         #botao para adicinar o salario
-        self.bt_f2 = Button(self.frame_2, text="Adicionar",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'))
+        self.bt_f2 = Button(self.frame_2, text="Adicionar",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'),command= self.addinvestimentos)
         self.bt_f2.place(relx=0.80, rely=0.73,relwidth=0.20,relheight=0.25)
-        self.bta_f2 = Button(self.frame_2, image=self.imagemlixo,bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'))
+
+        self.bta_f2 = Button(self.frame_2, image=self.imagemlixo,bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'),command=self.limpatelaframe2)
         self.bta_f2.place(relx=0.7, rely=0.73,relwidth=0.09,relheight=0.25)
         #botao para adicinar o investimentos
-        self.bt_f3 = Button(self.frame_3, text="Adicionar",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'))
+        self.bt_f3 = Button(self.frame_3, text="Adicionar",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'),command=self.addlazer)
         self.bt_f3.place(relx=0.80, rely=0.73,relwidth=0.20,relheight=0.25)
-        self.bta_f3 = Button(self.frame_3, image=self.imagemlixo,bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'))
+
+        self.bta_f3 = Button(self.frame_3, image=self.imagemlixo,bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'),command=self.limpatelaframe3)
         self.bta_f3.place(relx=0.70, rely=0.73,relwidth=0.09,relheight=0.25)
         #botao para adicinar o despesas
         self.bt_f4 = Button(self.frame_4, text="Adicionar",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'))
         self.bt_f4.place(relx=0.80, rely=0.73,relwidth=0.20,relheight=0.25)
-        self.bta_f4 = Button(self.frame_4, image=self.imagemlixo,bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'))
+
+        self.bta_f4 = Button(self.frame_4, image=self.imagemlixo,bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'),command=self.limpatelaframe4)
         self.bta_f4.place(relx=0.70, rely=0.73,relwidth=0.09,relheight=0.25)
         #botao para adicinar o improvisos
         self.bt_f5 = Button(self.frame_5, text="Adicionar",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'))
         self.bt_f5.place(relx=0.80, rely=0.73,relwidth=0.20,relheight=0.25)
-        self.bta_f5 = Button(self.frame_5, image=self.imagemlixo, bd=3, bg="ghost white",fg="gray1",font=('arial', 9, 'bold'))
+
+        self.bta_f5 = Button(self.frame_5, image=self.imagemlixo, bd=3, bg="ghost white",fg="gray1",font=('arial', 9, 'bold'),command=self.limpatelaframe5)
         self.bta_f5.place(relx=0.70, rely=0.73,relwidth=0.09,relheight=0.25)
         #botao para remover
         self.bt_f6 = Button(self.frame_6, text="Remover",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'))
@@ -185,20 +243,22 @@ class aplicacao():
         self.mimprovisos = Label(self.frame_5,text="x",bg='azure',fg="gray1",font=('arial', 9))
         self.mimprovisos.place(relx=0.80,rely=0.40)
     def lista(self):
-        self.listagasto = ttk.Treeview(self.frame_6, height=2, column=("coli1, coli2, coli3"))
-        self.listagasto.heading("#0",text="id")
-        self.listagasto.heading("#1",text="Descrição")
-        self.listagasto.heading("#2",text="Valor")
-        self.listagasto.heading("#3",text="Datas")
-        self.listagasto.column('#0',width=50)
-        self.listagasto.column('#1',width=200)
+        self.listagasto = ttk.Treeview(self.frame_6, height=3, column=("coli1", "coli2", "coli3","coli4"))
+        self.listagasto.heading("#0",text="")
+        self.listagasto.heading("#1",text="id")
+        self.listagasto.heading("#2",text="Descrição")
+        self.listagasto.heading("#3",text="Valor")
+        self.listagasto.heading("#4",text="Datas")
+        self.listagasto.column('#0',width=1)
+        self.listagasto.column('#1',width=50)
         self.listagasto.column('#2',width=200)
-        self.listagasto.column('#3',width=200)
+        self.listagasto.column('#3',width=125)
+        self.listagasto.column('#4',width=125)
         self.listagasto.place(relx=0.01,rely=0.02,relwidth=0.95,relheight=0.85)
 
         self.rolagem = Scrollbar(self.frame_6, orient='vertical')
         self.listagasto.configure(yscroll=self.rolagem.set)
         self.rolagem.place(relx=0.96, rely=0.02, relwidth=0.02,relheight=0.85)
-        
+       
 
 aplicacao()
