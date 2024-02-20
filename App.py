5219f6c743a88ad4e6294cdcdaba93f1a15b60cd
ErickMeucci <144945118+ErickMeucci@users.noreply.github.com>
2024-02-20 12:51:10 -0300

    Adicionado calculadora no programa
    foi adicionado a calculadora e varias melhorias de compilação

--- App.py
+++ App.py
@@ -1,10 +1,17 @@
 from tkinter import *
 from tkinter import ttk
 import sqlite3 as lite
+import subprocess
+from tkcalendar import DateEntry
+from tkinter import messagebox
+from datetime import datetime
+
+
 
 Janela = Tk()
 
 class Funcoes():
+    
     def limpatelaframe1(self):
         self.psentrada.delete(0, END)
         self.pientrada.delete(0, END)
@@ -14,19 +21,15 @@ class Funcoes():
     def limpatelaframe2(self):
         self.einvestimentos.delete(0, END)
         self.vinvestimentos.delete(0, END)
-        self.dinvestimentos.delete(0, END)
     def limpatelaframe3(self):
         self.elazer.delete(0, END)
         self.vlazer.delete(0, END)
-        self.dlazer.delete(0, END)
     def limpatelaframe4(self):
         self.edespesas.delete(0, END)
-        self.vdespesas.delete(0, END)
-        self.ddespesas.delete(0, END) 
+        self.vdespesas.delete(0, END) 
     def limpatelaframe5(self):
         self.eimprovisos.delete(0, END)
         self.vimprovisos.delete(0, END)
-        self.dimprovisos.delete(0, END) 
     def conecta_bd(self):
         self.conn = lite.connect("pagamentos.bd")
         self.cursor = self.conn.cursor(); print('conecta banco de dados')
@@ -42,39 +45,128 @@ class Funcoes():
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS porcentagens (salario INTEGER(10) NOT NULL, lazer INTEGER(10) NOT NULL, despesas INTEGER(10) NOT NULL, investimentos INTEGER(10) NOT NULL, imprevisto INTEGER(10) NOT NULL )""")
         self.conn.commit();print("banco de dados tabela criado")
         self.desconecta_bd()
+    def is_number(self, value):
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+    def is_valid_date(self, date_str):
+        try:
+            datetime.strptime(date_str, '%d/%m/%Y')
+            return True
+        except ValueError:
+            return False
     def addinvestimentos(self):
-        self.investimentos = self.einvestimentos.get()
-        self.investimentos1 = self.vinvestimentos.get()
-        self.investimentos2 = self.dinvestimentos.get()
+        investimentos = self.einvestimentos.get()
+        valor_investimentos = self.vinvestimentos.get()
+        data_investimentos = self.dinvestimentos.get()
+
+        if not investimentos or not valor_investimentos or not data_investimentos:
+            messagebox.showerror("Erro", "Preencha todos os campos do investimento.")
+            return
+
+        if not self.is_number(valor_investimentos):
+            messagebox.showerror("Erro", "O valor do investimento deve ser um número.")
+            return
+
+        if not self.is_valid_date(data_investimentos):
+            messagebox.showerror("Erro", "A data do investimento não é válida.")
+            return
+
         self.conecta_bd()
-        self.cursor.execute("""INSERT INTO pagamentos (descricao, valor, data) VALUES (?,?,?)""",(self.investimentos,self.investimentos1,self.investimentos2))
+        self.cursor.execute("""INSERT INTO pagamentos (descricao, valor, data) VALUES (?,?,?)""",
+                            (investimentos, valor_investimentos, data_investimentos))
         self.conn.commit()
-        self.desconecta_bd
+        self.desconecta_bd()
         self.selectlista()
         self.limpatelaframe2()
     def addlazer(self):
-        self.lazer1 = self.elazer.get()
-        self.lazer2 = self.vlazer.get()
-        self.lazer3 = self.dlazer.get()
+        lazer1 = self.elazer.get()
+        lazer2 = self.vlazer.get()
+        lazer3 = self.dlazer.get()
+        if not lazer1 or not lazer2 or not lazer3:
+            messagebox.showerror("Erro", "Preencha todos os campos do lazer.")
+            return
+        if not self.is_number(lazer2):
+            messagebox.showerror("Erro", "O valor do lazer deve ser um número.")
+            return
+
+        if not self.is_valid_date(lazer3):
+            messagebox.showerror("Erro", "A data do lazer não é válida.")
+            return
         self.conecta_bd()
-        self.cursor.execute("""INSERT INTO pagamentos (descricao, valor, data) VALUES (?,?,?)""",(self.lazer1,self.lazer2,self.lazer3))
+        self.cursor.execute("""INSERT INTO pagamentos (descricao, valor, data) VALUES (?,?,?)""",(lazer1, lazer2, lazer3))
         self.conn.commit()
-        self.desconecta_bd
+        self.desconecta_bd()
         self.selectlista()
         self.limpatelaframe3()
-    def selectlista(self):
+    def adddespesas(self):
+        despesa1 = self.edespesas.get()
+        despesa2 = self.vdespesas.get()
+        despesa3 = self.ddespesas.get()
+
+        if not despesa1 or not despesa2 or not despesa3:
+            messagebox.showerror("Erro", "Preencha todos os campos das despesas.")
+            return
+
+        if not self.is_number(despesa2):
+            messagebox.showerror("Erro", "O valor das despesas deve ser um número.")
+            return
+
+        if not self.is_valid_date(despesa3):
+            messagebox.showerror("Erro", "A data das despesas não é válida.")
+            return
+
+        self.conecta_bd()
+        self.cursor.execute("""INSERT INTO pagamentos (descricao, valor, data) VALUES (?,?,?)""",
+                            (despesa1, despesa2, despesa3))
+        self.conn.commit()
+        self.desconecta_bd()
+        self.selectlista()
+        self.limpatelaframe4()
+    def addimprovisos(self):
+        improvisos1 = self.eimprovisos.get()
+        improvisos2 = self.vimprovisos.get()
+        improvisos3 = self.dimprovisos.get()
+
+        if not improvisos1 or not improvisos2 or not improvisos3:
+            messagebox.showerror("Erro", "Preencha todos os campos dos improvisos.")
+            return
+
+        if not self.is_number(improvisos2):
+            messagebox.showerror("Erro", "O valor dos improvisos deve ser um número.")
+            return
+
+        if not self.is_valid_date(improvisos3):
+            messagebox.showerror("Erro", "A data dos improvisos não é válida.")
+            return
+
+        self.conecta_bd()
+        self.cursor.execute("""INSERT INTO pagamentos (descricao, valor, data) VALUES (?,?,?)""",
+                            (improvisos1, improvisos2, improvisos3))
+        self.conn.commit()
+        self.desconecta_bd()
+        self.selectlista()
+        self.limpatelaframe5()
+    def selectlista(self, selected_month=None):
         self.listagasto.delete(*self.listagasto.get_children())
         self.conecta_bd()
         listas = self.cursor.execute("""SELECT cod, descricao, valor, data FROM pagamentos ORDER BY data ASC;""")
         for i in listas:
             self.listagasto.insert("",END, values=i)
         self.desconecta_bd()
-    def calcula(self):
-        Janelacalculadora = Tk()
-        self.Janelacalculadora = Janelacalculadora
-        self.Janelacalculadora.geometry("300x500")
-        self.Janelacalculadora.title("Calculadora")
-        Janelacalculadora.mainloop()
+    def remover_item(self):
+        item_selecionado = self.listagasto.selection()
+        if not item_selecionado:
+            messagebox.showinfo("Aviso", "Selecione um item para remover.")
+            return
+        cod_selecionado = self.listagasto.item(item_selecionado)["values"][0]
+        self.conecta_bd()
+        self.cursor.execute("DELETE FROM pagamentos WHERE cod = ?", (cod_selecionado,))
+        self.conn.commit()
+        self.desconecta_bd()
+        self.selectlista() 
 
 class aplicacao(Funcoes):
     def __init__(self):
@@ -141,18 +233,25 @@ class aplicacao(Funcoes):
         self.bta_f5 = Button(self.frame_5, image=self.imagemlixo, bd=3, bg="ghost white",fg="gray1",font=('arial', 9, 'bold'),command=self.limpatelaframe5)
         self.bta_f5.place(relx=0.70, rely=0.73,relwidth=0.09,relheight=0.25)
         #botao para remover
-        self.bt_f6 = Button(self.frame_6, text="Remover",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'))
+        self.bt_f6 = Button(self.frame_6, text="Remover",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'), command=self.remover_item)
         self.bt_f6.place(relx=0.90, rely=0.90,relwidth=0.08,relheight=0.09)
         #botao calculadora
-        self.bt_f6 = Button(self.frame_6, text="Calculadora",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'),command=self.calcula)
-        self.bt_f6.place(relx=0.79, rely=0.90,relwidth=0.10,relheight=0.09)     
+        self.bt_f7 = Button(self.frame_6, text="Calculadora",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'), command=self.botao_calculadora)
+        self.bt_f7.place(relx=0.79, rely=0.90,relwidth=0.10,relheight=0.09) 
+        #entry de data
+        self.datageralt = Label(self.frame_6, text="Mês:",bg='azure',fg="gray1",font=('arial', 11, 'bold'))
+        self.datageralt.place(relx=0.57,rely=0.90,relheight=0.09)
+        self.datageral = DateEntry(self.frame_6, date_pattern='dd/mm/yyyy') 
+        self.datageral.place(relx=0.62, rely=0.90,relwidth=0.1,relheight=0.09)  
+        self.bt_f8 = Button(self.frame_6, text="OK",bd=3,bg="ghost white",fg="gray1",font=('arial', 9, 'bold'))
+        self.bt_f8.place(relx=0.725, rely=0.90,relwidth=0.04,relheight=0.09)
     def textosentradas(self):
         #introdiçao frame 1
         self.instrucao = Label(self.frame_1, text="Digite suas porcentagens:",bg='azure',fg="gray1",font=('arial', 11, 'bold'))
         self.instrucao.place(relx=0.005,rely=0.05)
         #salario frame 1
-        self.salario = Label(self.frame_1, text="Porcentagens do Salário:",bg='azure',fg="gray1",font=('arial', 9))
-        self.salario.place(relx=0.23,rely=0.05)
+        self.salario = Label(self.frame_1, text="Digite o Salário:",bg='azure',fg="gray1",font=('arial', 9))
+        self.salario.place(relx=0.29,rely=0.05)
         self.psentrada = Entry(self.frame_1,borderwidth=1,relief="solid")
         self.psentrada.place(relx=0.40,rely=0.05,relwidth=0.05)
         #investimento frame 1
@@ -174,7 +273,7 @@ class aplicacao(Funcoes):
         self.lazer = Label(self.frame_1, text="Porcentagens das Lazer:",bg='azure',fg="gray1",font=('arial', 9))
         self.lazer.place(relx=0.50,rely=0.05)
         self.plazer = Entry(self.frame_1,borderwidth=1,relief="solid")
-        self.plazer.place(relx=0.68,rely=0.05,relwidth=0.05)
+        self.plazer.place(relx=0.668,rely=0.05,relwidth=0.05)
 
         #entrada investimentos frame 2
         self.investimentos = Label (self.frame_2, text="Investimentos",bg='azure',fg="gray1",font=('arial', 11, 'bold'))
@@ -189,8 +288,8 @@ class aplicacao(Funcoes):
         self.vinvestimentos.place(relx=0.3,rely=0.6 )
         self.tipo = Label(self.frame_2,text="Data:",bg='azure',fg="gray1",font=('arial', 9))
         self.tipo.place(relx=0.70,rely=0.15)
-        self.dinvestimentos = Entry(self.frame_2)
-        self.dinvestimentos.place(relx=0.80,rely=0.15,relwidth=0.15 )
+        self.dinvestimentos = DateEntry(self.frame_2, date_pattern='dd/mm/yyyy')
+        self.dinvestimentos.place(relx=0.80,rely=0.15,relwidth=0.19)
         self.tipo = Label(self.frame_2,text="Meta:",bg='azure',fg="gray1",font=('arial', 9))
         self.tipo.place(relx=0.70,rely=0.40)
         self.minvestimentos = Label(self.frame_2,text="x",bg='azure',fg="gray1",font=('arial', 9))
@@ -209,8 +308,8 @@ class aplicacao(Funcoes):
         self.vlazer.place(relx=0.3,rely=0.6 )
         self.tipo = Label(self.frame_3,text="Data:",bg='azure',fg="gray1",font=('arial', 9))
         self.tipo.place(relx=0.70,rely=0.15)
-        self.dlazer = Entry(self.frame_3)
-        self.dlazer.place(relx=0.80,rely=0.15,relwidth=0.15 )
+        self.dlazer = DateEntry(self.frame_3, date_pattern='dd/mm/yyyy')
+        self.dlazer.place(relx=0.80,rely=0.15,relwidth=0.19 )
         self.tipo = Label(self.frame_3,text="Meta:",bg='azure',fg="gray1",font=('arial', 9))
         self.tipo.place(relx=0.70,rely=0.40)
         self.mlazer = Label(self.frame_3,text="x",bg='azure',fg="gray1",font=('arial', 9))
@@ -230,8 +329,8 @@ class aplicacao(Funcoes):
         self.vdespesas.place(relx=0.3,rely=0.6 )
         self.tipo = Label(self.frame_4,text="Data:",bg='azure',fg="gray1",font=('arial', 9))
         self.tipo.place(relx=0.70,rely=0.15)
-        self.ddespesas = Entry(self.frame_4)
-        self.ddespesas.place(relx=0.80,rely=0.15,relwidth=0.15 )
+        self.ddespesas = DateEntry(self.frame_4, date_pattern='dd/mm/yyyy')
+        self.ddespesas.place(relx=0.80,rely=0.15,relwidth=0.19 )
         self.tipo = Label(self.frame_4,text="Meta:",bg='azure',fg="gray1",font=('arial', 9))
         self.tipo.place(relx=0.70,rely=0.40)
         self.mdespesas = Label(self.frame_4,text="x",bg='azure',fg="gray1",font=('arial', 9))
@@ -250,8 +349,8 @@ class aplicacao(Funcoes):
         self.vimprovisos.place(relx=0.3,rely=0.6 )
         self.tipo = Label(self.frame_5,text="Data:",bg='azure',fg="gray1",font=('arial', 9))
         self.tipo.place(relx=0.70,rely=0.15)
-        self.dimprovisos = Entry(self.frame_5)
-        self.dimprovisos.place(relx=0.80,rely=0.15,relwidth=0.15 )
+        self.dimprovisos = DateEntry(self.frame_5, date_pattern='dd/mm/yyyy')
+        self.dimprovisos.place(relx=0.80,rely=0.15,relwidth=0.19 )
         self.tipo = Label(self.frame_5,text="Meta:",bg='azure',fg="gray1",font=('arial', 9))
         self.tipo.place(relx=0.70,rely=0.40)
         self.mimprovisos = Label(self.frame_5,text="x",bg='azure',fg="gray1",font=('arial', 9))
@@ -273,6 +372,9 @@ class aplicacao(Funcoes):
         self.rolagem = Scrollbar(self.frame_6, orient='vertical')
         self.listagasto.configure(yscroll=self.rolagem.set)
         self.rolagem.place(relx=0.96, rely=0.02, relwidth=0.02,relheight=0.85)
-       
+    def botao_calculadora(self):
+        caminho_calculadora = "C:\\Users\\Erick\\Desktop\\Controle financeiro\\calcular.py"
+        subprocess.Popen(["python", caminho_calculadora])
+   
        
 aplicacao()
